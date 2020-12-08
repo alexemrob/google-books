5889a7f1d67efe97a307fbef4653e2163fe858aa
Alexandria Robertson <67127042+alexemrob@users.noreply.github.com>
2020-12-08 13:09:05 -0600

    book search

---
+++ scripts/seedDB.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+    process.env.MONGODB_URI ||
+    "mongodb://localhost/googlebooks"
+);
+
+const bookSeed =
+{
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    description: "Harry Potter has no idea how famous he is. That's because he's being raised by his miserable aunt and uncle who are terrified Harry will learn that he's really a wizard, just as his parents were. But everything changes when Harry is summoned to attend an infamous school for wizards, and he begins to discover some clues about his illustrious birthright. From the surprising way he is greeted by a lovable giant, to the unique curriculum and colorful faculty at his unusual school, Harry finds himself drawn deep inside a mystical world he never knew existed and closer to his own noble destiny.",
+    image: "http://books.google.com/books/content?id=sazytgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    link: "http://books.google.com/books?id=sazytgAACAAJ&dq=title:The+Hunger+Games&hl=&source=gbs_api",
+}
+
+
+db.Book
+    .remove({})
+    .then(() => db.Book.collection.insertMany(bookSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
